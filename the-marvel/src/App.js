82b5c2ceff31208712bb7bb1b51ddea6c22831ef
Sharmilaa-14 <sharmilaa0514@gmail.com>
2025-11-20 11:35:14 +0530

    Upadated

--- the-marvel/src/App.js
+++ the-marvel/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 // Import your components
 import HomePage from "./componenets/home";
@@ -23,3 +23,4 @@ function App() {
 }
 
 export default App;
+
